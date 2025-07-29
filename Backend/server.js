7b168a8e9ad60b6cbca5ba5f01f31698adecd913
Savandi <savandikodithuwakku@gmail.com>
2025-07-29 14:39:31 +0530

    connect database

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,19 @@
 //entry point
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
+//console.log(process.env.MONGO_URI);
+
 app.listen(5000, () => {
-  console.log('Server is running on port 5000 Hello');
+  connectDB();
+  console.log('Server is running on port 5000');
 });
+//
