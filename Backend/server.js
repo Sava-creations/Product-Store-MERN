75ca2e183a8e092753107a972dbc8f48fcee1126
Savandi <savandikodithuwakku@gmail.com>
2025-07-29 14:03:57 +0530

    Initialize server

---
+++ Backend/server.js
@@ -0,0 +1,11 @@
+//entry point
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000 Hello');
+});
