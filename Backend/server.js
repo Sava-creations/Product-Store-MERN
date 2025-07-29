a74013a6416d0de7712148a017047366ddda223d
Savandi <savandikodithuwakku@gmail.com>
2025-07-29 20:39:10 +0530

    POST method

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Server is ready');
+
+app.use(express.json()); // Middleware allows us to parse JSON request bodies
+
+app.post('/api/products',async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if(!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: 'Please fill all the fields' });
+  }
+  const newProduct = new Product(product) // create a new product instance
+
+    try {
+        await newProduct.save(); // save the product to the database
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.log
+        res.status(500).json({ success: false, message: 'Server error' });
+    }
 });
+
 
 //console.log(process.env.MONGO_URI);
 
