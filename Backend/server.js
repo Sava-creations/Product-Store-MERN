bc7f7ade6cf1942ca6f5546dd707b2e56c97d4ce
Savandi <savandikodithuwakku@gmail.com>
2025-08-05 07:49:58 +0530

    update PORT for better practice

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ import productRoutes from './routes/product.route.js';
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
+const PORT =process.env.PORT || 5000; // from env env file we re using PORT if we forgot to set it, it will default to 5000
 
 app.use(express.json()); // Middleware allows us to parse JSON request bodies
 
@@ -14,7 +15,7 @@ app.use('/api/products', productRoutes); // Use 1 route
 //this part will added to all HTTP requests to the product routes / means /api/products /:id means /api/products/:id
 //console.log(process.env.MONGO_URI);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log('Server is running on port 5000');
 });
